99477b4d6d2e0113579f4e1380cc4a845caae19c
曹智铭 <xiaocao162020@xiaocao.onmicrosoft.com>
2021-10-13 21:15:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: '凛空の小屋 服务器状态监控',
+  SiteName: '曹智铭的网站 状态监控',
 
   // 站点链接
   SiteUrl: '/',
@@ -14,22 +14,22 @@ window.Config = {
   ],
 
   // 是否显示监测站点的链接
-  ShowLink: false,
+  ShowLink: true,
 
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 30,
+  CountDays: 50,
 
   // 导航栏菜单
   Navi: [
     {
-      text: '晓空blog',
-      url: 'https://blog.moeworld.tech/'
+      text: '曹智铭的网站',
+      url: 'https://blog.caozm.tk/'
     },
     {
-      text: '关于',
-      url: 'http://about.rcraft.ml'
+      text: '邮件',
+      url: 'mailto:caozm@caozm.tk'
     }
   ]
 };
